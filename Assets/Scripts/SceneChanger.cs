a3e3d203344589bb1cc534b5d539bdd3e5efdde0
Irina Mishina <irinamishina131@gmail.com>
2025-01-15 17:00:52 -0500

    UI buttons coded

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToLevel01()
+    {
+        SceneManager.LoadScene("Level01-Test");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GoToHelp()
+    {
+        SceneManager.LoadScene("HelpMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
