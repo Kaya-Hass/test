0b0a29c0e5aa13959c18f2c6891daa804dcb6792
Irina Mishina <irinamishina131@gmail.com>
2025-01-18 20:44:46 -0500

    patrolling code done

---
+++ Assets/Scripts/PatrolEnemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public GameObject enemy;
+    Rigidbody2D rb;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = pointB.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, pointB.transform.position) < 0.1f)
+        {
+            rb.linearVelocity = new Vector2(0, -speed);
+        }
+        else if(Vector2.Distance(transform.position, pointA.transform.position) < 0.1f)
+        {
+            rb.linearVelocity = new Vector2(0, speed);
+        }
+
+    }
+}
