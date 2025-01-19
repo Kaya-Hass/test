41b854def806553caf86444481430c21565134ec
Irina Mishina <irinamishina131@gmail.com>
2025-01-18 22:48:23 -0500

    max value of sleepbar set through editor

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -17,6 +17,7 @@ public class PlayerInteractions : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        sleepBar.maxValue = timer;
         deathPanel.SetActive(false);
         safePanel.SetActive(false);
         puzzlePanel.SetActive(false);
